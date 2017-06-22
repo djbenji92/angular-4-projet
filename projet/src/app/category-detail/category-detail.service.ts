caa948bc409ff9876b94fd5caba1a8a502223310
djbenji92 <benjamin.rouville@gmail.com>
2017-06-22 11:51:52 +0200

    resolve conflict

--- projet/src/app/category-detail/category-detail.service.ts
+++ projet/src/app/category-detail/category-detail.service.ts
@@ -8,23 +8,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CategoryDetailService {
 
-    private url = 'https://developers.zomato.com/api/v2.1/categories';
-    private key = "378c1121eca87ac3751004d5ff0f104a";
+    private url = 'http://localhost:3000/categories';
 
     constructor(private http: Http, private RequestOptions: RequestOptions) {
      }
 
     getCategories(){
-        let headers = new Headers();
-        
-        headers.append('user-key', '652f8085982c360f56e717c7b8bbe258')
-
-        let options = new RequestOptions({ headers: headers });
-
-        return this.http.get(this.url, options)
-                    .map((res) => res.json());
-
-        
+        return this.http.get(this.url)
+                    .map((res) => res.json()); 
     }
 
 }
